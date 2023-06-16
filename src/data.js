539cedff01ff32d0e203bfbd7d7563a8847babf6
ManausK <1905246@kiit.ac.in>
2023-06-16 13:04:44 +0530

    pure slider done

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import image1 from "./assets/model2.png";
+import image2 from "./assets/models.png";
+import image3 from "./assets/model4.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: image1,
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: image2,
+    title: "SPRING COLLECTION",
+    desc: "THE COLLECTION YOU WANTED IS HERE! GET FLAT 25% OFF FOR FIRST ORDER.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: image3,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET MIN 20% OFF FOR EVERY PRODUCT.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+  },
+];
