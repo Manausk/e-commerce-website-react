06f128012ae45c2fa2e56308f62bf58c0c415f69
ManausK <1905246@kiit.ac.in>
2023-06-16 17:03:52 +0530

    shopping categories and product list

--- src/data.js
+++ src/data.js
@@ -29,17 +29,52 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://images.pexels.com/photos/2887767/pexels-photo-2887767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     title: "SHIRT STYLE!",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "LOUNGEWEAR LOVE",
+    img: "https://images.pexels.com/photos/4494671/pexels-photo-4494671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "CASUALWEAR LOVE",
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    title: "LIGHT JACKETS",
+    img: "https://images.pexels.com/photos/1879877/pexels-photo-1879877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "WINTER SHADES",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://staticinm2.ishalife.com/in/pub/media/catalog/product/cache/7ae7abcb6e552a4027ad83c909021753/0/4/0401148_c.jpg",
+  },
+  {
+    id: 3,
+    img: "https://staticinm2.ishalife.com/in/pub/media/klevu_images/471X560/0/4/0401144.jpg",
+  },
+  {
+    id: 4,
+    img: "https://staticinm2.ishalife.com/in/pub/media/catalog/product/cache/7ae7abcb6e552a4027ad83c909021753/0/4/0401150.jpg",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://5.imimg.com/data5/SELLER/Default/2020/10/CD/IZ/IY/12789060/mens-jackets.jpg",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
